900636eb4960dc902d0e52447924c8a7e9878bb6
NicoFJCruz <nicofjcruz@gmail.com>
2023-05-30 15:57:16 -0300

    root

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Signin from "./components/Signin";
+import MoviePopular from "./components/Popular";
+import Search from "./components/Search";
+import Content from "./components/Content";
+import Favorites from "./components/Favorites";
+import Home from "./components/Home";
+import UsersNMDB from "./components/UsersNMDB";
+import IndividualUser from "./commons/IndividualUser";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState("");
+  const [user, setUser] = useState({});
+  const [favorites, setFavorites] = useState({});
 
+  useEffect(() => {
+    const userLog = JSON.parse(localStorage.getItem("user")) || {};
+    setUser(userLog);
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Navbar user={user} setUser={setUser} />
 
-export default App
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route
+          path="/:category/popular"
+          element={
+            <MoviePopular
+              setSearchResult={setSearchResult}
+              setSearch={setSearch}
+              search={search}
+            />
+          }
+        />
+        <Route
+          path="/:category/search"
+          element={<Search searchResult={searchResult} search={search} />}
+        />
+        <Route
+          path="/:category/favorites"
+          element={<Favorites user={user} />}
+        />
+        <Route
+          path="/:category/:id"
+          element={
+            <Content
+              user={user}
+              favorites={favorites}
+              setFavorites={setFavorites}
+            />
+          }
+        />
+        <Route path="/users" element={<UsersNMDB user={user} />} />
+        <Route path="/user/:id" element={<IndividualUser />} />
+        <Route path="*" element={<Home user={user} />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
