5d47b0c8bbfd1502829b075b1cecbc2002e569a8
NicoFJCruz <nicofjcruz@gmail.com>
2023-05-30 14:55:02 -0300

    config

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+const config = {
+    key: "7f004ce8cea63816fbec45654e8152b1",
+    url: "https://api.themoviedb.org/3/",
+    image: "https://image.tmdb.org/t/p/w185",
+    image_large: "https://image.tmdb.org/t/p/w342",
+  };
+  
+  module.exports = config;
+  
